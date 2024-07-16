2974e3424b9121e8518eb7e13d2ef76ac822908a
or orsatti <antonio@10play.dev>
2024-07-16 14:11:13 +0300

    feat(treesitter): alter whichkey to show context command

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -6,7 +6,7 @@ return { -- Useful plugin to show you pending keybinds.
 
     -- Document existing key chains
     require('which-key').register {
-      ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+      ['<leader>c'] = { name = '[C]ode/[C]ontext', _ = 'which_key_ignore' },
       ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
       ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
       ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
